a397cd7c81a5ad4b450e6a21b82ee47c7320c881
codewithsaleh <amransharahap@gmail.com>
2025-11-15 13:21:54 +0700

    Initial commit: Setup HTML + Tailwind portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './*.html',  // Scan semua HTML di root
+    './src/**/*.{html,js}'  // Kalo lo punya folder src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
